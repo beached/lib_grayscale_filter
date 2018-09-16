360d99d711aa232c9bbfde806a1d029b243a2886
Darrell Wright <dwright-git@dawdevel.ca>
2018-09-15 22:32:50 -0400

    .

--- src/filterdawgs.cpp
+++ src/filterdawgs.cpp
@@ -41,11 +41,11 @@ namespace daw {
 			GenericImage<rgb3> FilterDAWGS::filter( GenericImage<rgb3> const &input_image ) {
 
 			std::vector<uint32_t> keys{};
-			keys.reserve( input_image.size( ) );
+			keys.resize( input_image.size( ) );
 			// Create a unique_values item for each distinct grayscale item in image
 			// and then set count to zero
 			// no parallel to unique_values
-			std::transform( input_image.begin( ), input_image.end( ), std::back_inserter( keys ), []( auto rgb ) {
+			daw::algorithm::parallel::transform( input_image.begin( ), input_image.end( ), keys.begin( ), []( auto rgb ) {
 				return daw::imaging::FilterDAWGS::too_gs( rgb );
 			});
 
@@ -61,7 +61,7 @@ namespace daw {
 
 				return image_output;
 			}
-			std::sort( keys.begin(), keys.end( ) );
+			daw::algorithm::parallel::sort( keys.begin(), keys.end( ) );
 			keys.erase( std::unique( keys.begin( ), keys.end( ) ), keys.end( ) );
 
 			auto const inc = static_cast<float>( keys.size( ) ) / 256.0f;
@@ -75,66 +75,11 @@ namespace daw {
 
 			GenericImage<rgb3> output_image{input_image.width( ), input_image.height( )};
 
-			// TODO: make parallel
 			daw::algorithm::parallel::transform( input_image.cbegin( ), input_image.cend( ), output_image.begin( ),
 			                [&value_pos]( auto rgb ) { return value_pos[FilterDAWGS::too_gs( rgb )]; } );
 
 			return output_image;
 		}
-
-
-		#if false
-		/////
-		GenericImage<rgb3> FilterDAWGS::filter_old( GenericImage<rgb3> const &input_image ) {
-
-			std::unordered_set<uint32_t> unique_values{};
-
-			// Create a unique_values item for each distinct grayscale item in image
-			// and then set count to zero
-			// no parallel to unique_values
-			std::transform( input_image.begin( ), input_image.end( ), std::inserter( unique_values, unique_values.end( ) ), []( auto rgb ) {
-				return daw::imaging::FilterDAWGS::too_gs( rgb );
-			});
-
-			// If we must compress as there isn't room for number of grayscale items
-			if( unique_values.size( ) <= 256 ) {
-				std::cerr << "Already a grayscale image or has enough room for all "
-				             "possible values and no compression needed:"
-				          << unique_values.size( ) << std::endl;
-				GenericImage<rgb3> image_output{input_image.width( ), input_image.height( )};
-
-				std::transform( input_image.cbegin( ), input_image.cend( ), image_output.begin( ),
-				                []( rgb3 const &rgb ) { return static_cast<uint8_t>( rgb.too_float_gs( ) ); } );
-
-				return image_output;
-			}
-
-			auto const keys = [&unique_values]( ) {
-				std::vector<uint32_t> result{};
-				result.reserve( unique_values.size( ) );
-				std::copy( unique_values.cbegin( ), unique_values.cend( ), std::back_inserter( result ) );
-				std::sort( result.begin( ), result.end( ) );
-				return result;
-			}( );
-
-			auto const inc = static_cast<float>( keys.size( ) ) / 256.0f;
-
-			std::unordered_map<uint32_t, uint8_t> value_pos{};
-			value_pos.reserve( keys.size( ) );
-
-			for( size_t n = 0; n < keys.size( ); ++n ) {
-				value_pos.emplace( keys[n], static_cast<uint8_t>( static_cast<float>( n ) / inc ) );
-			}
-
-			GenericImage<rgb3> output_image{input_image.width( ), input_image.height( )};
-
-			// TODO: make parallel
-			std::transform( input_image.cbegin( ), input_image.cend( ), output_image.begin( ),
-			                [&value_pos]( auto rgb ) { return value_pos[FilterDAWGS::too_gs( rgb )]; } );
-
-			return output_image;
-		}
-		#endif
 
 #ifdef DAWFILTER_USEPYTHON
 		void FilterDAWGS::register_python( std::string const nameoftype ) {
